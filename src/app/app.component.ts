924952152cc93fa99fba397e07ce9cd0bf3596c5
AkosBartus <bartusakos0@gmail.com>
2024-04-26 09:04:30 +0200

    projects component with HTML CSS created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,13 +5,14 @@ import { HomeComponent } from "./home/home.component";
 import { StoryComponent } from "./story/story.component";
 import { QuoteComponent } from "./quote/quote.component";
 import { WorkflowComponent } from "./workflow/workflow.component";
+import { ProjectsComponent } from "./projects/projects.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
-    imports: [RouterOutlet, NavbarComponent, HomeComponent, StoryComponent, QuoteComponent, WorkflowComponent]
+    imports: [RouterOutlet, NavbarComponent, HomeComponent, StoryComponent, QuoteComponent, WorkflowComponent, ProjectsComponent]
 })
 export class AppComponent {
   title = 'portfolio';
